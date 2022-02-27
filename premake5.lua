278769c977fba0d78baeb08d56f97778aa81287a
ai21m027 <ai21m027@technikum-wien.at>
2022-02-27 03:53:54 +0100

    Finished Premake Video -> Generated Premake Script

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Sunshine"
+	architecture "x64"
+	
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Sunshine"
+	location "Sunshine"
+	kind "SharedLib"
+	language "C++"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+	
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+	
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		
+		defines{
+			"SU_PLATFORM_WINDOWS",
+			"SU_BUILD_DLL",
+		}
+		
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+	
+	filter "configurations:Debug"
+		defines "SU_DEBUG"
+		symbols "On"
+	filter "configurations:Release"
+		defines "SU_RELEASE"
+		optimize "On"
+	filter "configurations:Dist"
+		defines "SU_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	
+	language "C++"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+	
+	includedirs
+	{
+		"Sunshine/vendor/spdlog/include",
+		"Sunshine/src"
+	}
+	
+	links
+	{
+		"Sunshine"
+	}
+	
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		
+		defines{
+			"SU_PLATFORM_WINDOWS"
+		}
+		
+	
+	filter "configurations:Debug"
+		defines "SU_DEBUG"
+		symbols "On"
+	filter "configurations:Release"
+		defines "SU_RELEASE"
+		optimize "On"
+	filter "configurations:Dist"
+		defines "SU_DIST"
+		optimize "On"
+
+
+
+
